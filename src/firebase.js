9dc05399cf5da7010cdafa25d235c63e179d5ace
JC <j.c.ashley4363@gmail.com>
2023-02-28 19:32:48 -0600

    changed name

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,3 @@
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDzHzlEskRF8REDDE6isgOXp8V3nOOQzcI",
-//     authDomain: "netflix-clone-84544.firebaseapp.com",
-//     projectId: "netflix-clone-84544",
-//     storageBucket: "netflix-clone-84544.appspot.com",
-//     messagingSenderId: "435774344564",
-//     appId: "1:435774344564:web:de51406cb99207e213b021"
-// };
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -16,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA2JDvweGn0K77moO-DX1JA6ETtcNnWP0A",
-    authDomain: "netflix-clone-ba8d5.firebaseapp.com",
-    projectId: "netflix-clone-ba8d5",
-    storageBucket: "netflix-clone-ba8d5.appspot.com",
-    messagingSenderId: "126703964708",
-    appId: "1:126703964708:web:ec935680a0f9cc6251beb1",
-    measurementId: "G-F6Z4ZVXTGN",
-}
+    apiKey: "AIzaSyAubZzzJv1puL-pF5wuEV0_HuA3Zh_sO_E",
+    authDomain: "tiasonlinemovies.firebaseapp.com",
+    projectId: "tiasonlinemovies",
+    storageBucket: "tiasonlinemovies.appspot.com",
+    messagingSenderId: "237398087320",
+    appId: "1:237398087320:web:a80bebaeaa58f49f86a2ed",
+    measurementId: "G-65B43JVC55"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
